5ece86ccbf5a3dbc3b7d946fae4f851ac7ecc8bc
Noa Lungström <noa.lungstrom@elev.karlstadsglobala.se>
2022-12-01 11:59:04 +0100

    fortsatt arbete på character movement.
    Skript fungerar inte. Konstigt felmeddelande.  Direction följer inte kamera än.

--- Uf_Game/Assets/ThirdPersonMovement.cs
+++ Uf_Game/Assets/ThirdPersonMovement.cs
@@ -4,23 +4,27 @@ using UnityEngine;
 
 public class ThirdPersonMovement : MonoBehaviour
 {
-   public Character Controller controller;
+   public CharacterController controller;
    
    public float speed = 6f;
-    
-   
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
 
     // Update is called once per frame
     void Update()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = input.GetAxisRaw("Vertical");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
+        if (direction.magnitude >= 0.1)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            float angle = Mathf.SmoothDampAngle(transform.eulerlAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            controller.Move(direction * speed * Time.deltaTime);
+        }
     }
 }
