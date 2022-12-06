e581e2632809628c75d87462b702da367fe69853
Noa Lungström <noa.lungstrom@elev.karlstadsglobala.se>
2022-12-06 10:39:55 +0100

    Progressing on character controls
    Jump script is started. Fucks up colissions

--- Uf_Game/Assets/ThirdPersonMovement.cs
+++ Uf_Game/Assets/ThirdPersonMovement.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class ThirdPersonMovement : MonoBehaviour
 {
    public CharacterController controller;
-   
+    public Transform cam;
+
+    public float jumpForce = 10;
+    public float gravity = -9.81f;
+    float velocity;
+
    public float speed = 6f;
 
-    public float turnSmoothTime = 0.1f;
-    float turnSmoothVelocity;
+   
 
     // Update is called once per frame
     void Update()
@@ -20,11 +24,23 @@ public class ThirdPersonMovement : MonoBehaviour
 
         if (direction.magnitude >= 0.1)
         {
+<<<<<<< Updated upstream
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
             float angle = Mathf.SmoothDampAngle(transform.EulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
+=======
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+>>>>>>> Stashed changes
 
-            controller.Move(direction * speed * Time.deltaTime);
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward; 
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
+        velocity += gravity * Time.deltaTime;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            velocity = jumpForce;
+        }
+        transform.Translate(new Vector3(0, velocity, 0) * Time.deltaTime);
     }
 }
